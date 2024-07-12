88487e2e9d8e4016995476dbd45c2bf52fea0160
japygo <hyunnjinn@gmail.com>
2024-07-12 17:17:48 +0900

    다형성 활용하기

--- src/main/java/cleancode/minesweeper/tobe/cell/CellSnapshot.java
+++ src/main/java/cleancode/minesweeper/tobe/cell/CellSnapshot.java
@@ -32,6 +32,10 @@ public class CellSnapshot {
         return new CellSnapshot(CellSnapshotStatus.NUMBER, nearbyLandMineCount);
     }
 
+    public boolean isSameStatus(CellSnapshotStatus cellSnapshotStatus) {
+        return this.status == cellSnapshotStatus;
+    }
+
     public static CellSnapshot ofUnchecked() {
         return new CellSnapshot(CellSnapshotStatus.UNCHECKED, 0);
     }
@@ -56,5 +60,4 @@ public class CellSnapshot {
     public int hashCode() {
         return Objects.hash(getStatus(), getNearbyLandMineCount());
     }
-
 }
